ed9b56debcc8382ca4e8e094eace20fed0489a71
gcvin <gc.vin@qq.com>
2024-12-21 09:18:13 +0000

    Deploying to gh-pages from @ gcvin/monorepo-app@4b81089227cc271f3e82436c0d253f7b42c45308 🚀

--- assets/chunks/with-install.81cb298e.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./create.4fdb5152.js";function a(e){return e.install=m=>{const{name:n}=e;n&&(m.component(n,e),m.component(r(`-${n}`),e))},e}export{a as w};
---
+++ assets/chunks/with-install.c51a5675.js
@@ -0,0 +1,1 @@
+import{r}from"./create.9c566424.js";function a(e){return e.install=m=>{const{name:n}=e;n&&(m.component(n,e),m.component(r(`-${n}`),e))},e}export{a as w};
