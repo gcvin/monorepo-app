66075edc9220d2432e292ae00b7601f51547b747
gcvin <gc.vin@qq.com>
2024-12-28 04:20:52 +0000

    Deploying to gh-pages from @ gcvin/monorepo-app@6a5e97b39df5519a45ac9e1e34f3e9ae281d791e 🚀

---
+++ assets/chunks/with-install.BMI48HX4.js
@@ -0,0 +1,1 @@
+import{o as t}from"./create.CwGpzbaT.js";function c(e){return e.install=m=>{const{name:n}=e;n&&(m.component(n,e),m.component(t(`-${n}`),e))},e}export{c as w};
--- assets/chunks/with-install.c51a5675.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./create.9c566424.js";function a(e){return e.install=m=>{const{name:n}=e;n&&(m.component(n,e),m.component(r(`-${n}`),e))},e}export{a as w};
